ae46f171dca3bd8a1a1833a5e7654dfe4a07ab2c
Brad Walbeck <brad.walbeck@gmail.com>
2015-11-24 11:37:05 -0700

    removed unused 'using' statements.

--- ConwaysGameOfLife/ConwaysGameOfLifeTest/CellTest.cs
+++ ConwaysGameOfLife/ConwaysGameOfLifeTest/CellTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ConwaysGameOfLife;
+﻿using ConwaysGameOfLife;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConwaysGameOfLifeTest
--- ConwaysGameOfLife/ConwaysGameOfLifeTest/LifeCycleTest.cs
+++ ConwaysGameOfLife/ConwaysGameOfLifeTest/LifeCycleTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ConwaysGameOfLife;
+﻿using ConwaysGameOfLife;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
